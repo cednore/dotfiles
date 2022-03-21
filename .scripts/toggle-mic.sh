edbc8072a53be0cfb1e3c07f3bbbcca2de285493
cednore <cednore@gmail.com>
2022-03-21 01:40:36 -0700

    ✨ toggle-mic script

---
+++ .scripts/toggle-mic.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+state=`amixer set Capture toggle | gawk 'match($0, /Front Left.*\[(.*)\]/, a) {print a[1]}'`
+if [ $state = "off" ]; then
+    icon="audio-input-microphone-muted-symbolic"
+else
+    icon="audio-input-microphone-symbolic"
+fi
+notify-send --hint=int:transient:1 -i $icon "Mic switched: $state"
