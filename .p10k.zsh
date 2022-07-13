21143ae58191b7b04e5ce63a770a3be3de227f4a
cednore <cednore@gmail.com>
2022-07-13 09:51:48 -0700

    ⚡ enable shell integration in p10k

--- .p10k.zsh
+++ .p10k.zsh
@@ -1630,6 +1630,9 @@
   # really need it.
   typeset -g POWERLEVEL9K_DISABLE_HOT_RELOAD=true
 
+  # Enable shell integration
+  typeset -g POWERLEVEL9K_TERM_SHELL_INTEGRATION=true
+
   # If p10k is already loaded, reload configuration.
   # This works even with POWERLEVEL9K_DISABLE_HOT_RELOAD=true.
   (( ! $+functions[p10k] )) || p10k reload
